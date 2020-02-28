026edb0e4d967398a7cf2f897daa00d628ab6171
robert <rvasquez@robotnik.es>
2020-02-28 11:39:21 +0100

    Changed the behavior of the LEDs

--- robotnik_leds_control/src/leds_control.py
+++ robotnik_leds_control/src/leds_control.py
@@ -18,7 +18,7 @@ class LedsControl:
 
 		rospy.loginfo("Waiting Arduino Lighting Signaling (ALS module) ...")		
 
-        # Wait for the service offered by the Arduino hardware
+        # Check if ALS module is already
 		rospy.wait_for_service('/leds_driver/command')
 
 		rospy.loginfo("ALS module activated, node already! ")
@@ -30,7 +30,6 @@ class LedsControl:
 		rospy.wait_for_service('/leds_driver/command')
 		leds_driver_client = rospy.ServiceProxy('/leds_driver/command', SetLeds)
 
-
 		vel_x = msg.linear.x
 		vel_y = msg.linear.y
 		vel_z = msg.angular.z
@@ -38,10 +37,18 @@ class LedsControl:
 
 		if vel_x == 0 and vel_y == 0 and vel_z == 0:
 
+			#Important: First disable effects, then activate effects
+			
 			print("Robot stops!")
 
-			command.leds_name = "all_leds"
-			command.state = "MOVING"
+			
+			command.leds_name = "front_right_leds"
+			command.state = "TURN_LEFT"
+			command.enable = False
+			leds_driver_client(command)
+
+			command.leds_name = "rear_right_leds"
+			command.state = "TURN_RIGHT"
 			command.enable = False
 			leds_driver_client(command)
 
@@ -49,31 +56,65 @@ class LedsControl:
 			command.state = "STOP"
 			command.enable = True
 			leds_driver_client(command)
+
+			#command.leds_name = "all_leds"
+			#command.state = "MOVING"
+			#command.enable = False
+			#leds_driver_client(command)
+
+			#command.leds_name = "all_leds"
+			#command.state = "STOP"
+			#command.enable = True
+			#leds_driver_client(command)
 
 		else:
 
 			print("Robot moves!")
-			command.leds_name = "all_leds"
-			command.state = "MOVING"
-			command.enable = True
-			leds_driver_client(command)
 
 			command.leds_name = "all_leds"
 			command.state = "STOP"
 			command.enable = False
 			leds_driver_client(command)
+
+
+			command.leds_name = "front_right_leds"
+			command.state = "TURN_LEFT"
+			command.enable = True
+			leds_driver_client(command)
+
+			command.leds_name = "rear_right_leds"
+			command.state = "TURN_RIGHT"
+			command.enable = True
+			leds_driver_client(command)
+
+
+
+			#command.leds_name = "all_leds"
+			#command.state = "STOP"
+			#command.enable = False
+			#leds_driver_client(command)
+
+			#command.leds_name = "all_leds"
+			#command.state = "MOVING"
+			#command.enable = True
+			#leds_driver_client(command)
+
+		
+
+
 
 
 
 def main():
 
 	leds_control = LedsControl()
+
+
 	
 
 	while not rospy.is_shutdown():
 
 		dummy = 1        
-
 
 
 
