88759c22736b7c62c12c15a65f268b933a463d1c
robert <rvasquez@robotnik.es>
2020-03-05 10:17:27 +0100

    Implemented common functions for all effects classes

--- arduino_robotnik_leds/CommonEffect.cpp
+++ arduino_robotnik_leds/CommonEffect.cpp
@@ -25,17 +25,141 @@
   }
 
 
-  void CommonEffect::beginPixels(void){
-    
-        pixels->begin();
-        pixels->clear();
-        pixels->show();
-  
-  }
 
   void CommonEffect::setPixelsColor(int led, int color_R, int color_G, int color_B){
     
       pixels->setPixelColor(led, pixels->Color(color_R, color_G, color_B));  
   
   }
+
+
+
+   // Fill and show a led strip from start_led to end_led, both included. First led of the strip starts in 1.
+
+  void CommonEffect::showFillPixels(int color_R, int color_G, int color_B, int start_led, int end_led){
+    
+       //Example: start_led = 2, end_led =  4  There are 4-2+1 = 3 pixels involved
+      int count_led =  end_led - start_led + 1; 
+      
+      pixels->fill(pixels->Color(color_R, color_G, color_B), start_led - 1, count_led);
+      pixels->show();
+      
+  }
+
+
+
+
+  void CommonEffect::assign_id(String id_assigned){
+    
+      this->id_assigned = id_assigned; 
+  }
+
+
+  
+  void CommonEffect::updateLedZone(struct LedProperties effect_config){
+
+      /* Auxiliar local variables */    
+      int old_start_led = this -> effect_config.start_led;
+      int old_end_led = this -> effect_config.end_led;
+      int new_start_led = effect_config.start_led;
+      int new_end_led = effect_config.end_led;
+      
+      int start_difference = new_start_led - old_start_led;
+      int end_difference =   old_end_led - new_end_led;
+  
+  
+      // If the distance beetween new start led and old start led is positive, clean the difference
+      if (start_difference > 0){
+  
+          fillPixels(0,0,0, old_start_led-1, start_difference);
+          showPixels();
+      }
+  
+      // Same logic with the end leds
+      if (end_difference  > 0){
+  
+          fillPixels(0,0,0, new_end_led , end_difference );
+          showPixels();
+      } 
+    
+  }
+
+
+  void CommonEffect::updateEffectConfig(struct LedProperties effect_config){
+
+          this -> effect_config = effect_config;
+
+  }
+
+
+  void CommonEffect::resetEffectConfig(void){
+
+        struct LedProperties default_effect_config;
+        
+        //Reset variables to default values
+        this -> effect_config = default_effect_config;
+    
+  }
+
+  void CommonEffect::cleanCurrentLedZone(void){
+
+        int start_led = this -> effect_config.start_led;
+        int end_led = this -> effect_config.end_led;
+
+        // Clean led zone
+        showFillPixels(0,0,0, start_led, end_led);
+ 
+  }
+
+  void CommonEffect::removeEffect(void){
+
+        cleanCurrentLedZone();
+        resetEffectConfig();
+  }
+
+
+  uint8_t CommonEffect::update(struct LedProperties effect_config){
+
+          updateFlag = true;
+  
+          // Check if the ID corresponds to the ID assigned to an instance of this class
+          if(id_assigned.equals(effect_config.id)){
+
+              if(effect_config.enabled) {
+                                  
+                  // Check if the led zone has changed
+                  updateLedZone(effect_config);
+                  
+                  // Update values in the object
+                  updateEffectConfig(effect_config);
+              }
+              
+              else{
+
+                  // Clean values of the object, restored default values
+                  removeEffect();
+              }
+                
+          }
+        
+      return -1;
+  }
+
+  bool CommonEffect::checkUpdates(void){
+
+      bool isUpdated;
+
+      if(updateFlag)
+          isUpdated = true;
+      else
+          isUpdated = false;
+
+      //Clear updateFlag
+      updateFlag = false;
+          
+      return isUpdated;
+  }
+
+    
+
   
--- arduino_robotnik_leds/CommonEffect.h
+++ arduino_robotnik_leds/CommonEffect.h
@@ -5,10 +5,48 @@
   #include <Arduino.h>
   #include <Adafruit_NeoPixel.h>
 
+
+  // General properties used by all led effects
+ 
+  struct LedProperties{
+
+  LedProperties(): 
+      id(""),
+      color_R(0),
+      color_G(0),
+      color_B(0),
+      start_led(0),
+      end_led(0),
+      ms_on(0),
+      ms_off(0),
+      direction(""),
+      speed(0),
+      sleep(0),
+      enabled(false) {}
+  
+  String   id;
+  uint8_t  color_R;
+  uint8_t  color_G;
+  uint8_t  color_B;
+  uint16_t start_led;
+  uint16_t end_led;
+  uint16_t ms_on = 0;
+  uint16_t ms_off = 0;
+  String   direction = "right";
+  uint16_t speed = 0;
+  uint16_t sleep = 0;
+  bool     enabled;
+  
+  };
+
+  
+
   class CommonEffect {
     
     
     private:
+
+        bool updateFlag = false;
 
         
     public:
@@ -18,46 +56,27 @@
 
         CommonEffect(Adafruit_NeoPixel &pixels);
 
+  
+        struct LedProperties effect_config;
 
-        // General properties used by all led effects
-       
-        struct LedProperties{
-    
-        LedProperties(): 
-            id(""),
-            color_R(0),
-            color_G(0),
-            color_B(0),
-            start_led(0),
-            end_led(0),
-            ms_on(0),
-            ms_off(0),
-            direction(""),
-            speed(0),
-            sleep(0),
-            enabled(false) {}
-        
-        String   id;
-        uint8_t  color_R;
-        uint8_t  color_G;
-        uint8_t  color_B;
-        uint16_t start_led;
-        uint16_t end_led;
-        uint16_t ms_on = 0;
-        uint16_t ms_off = 0;
-        String   direction = "right";
-        uint16_t speed = 0;
-        uint16_t sleep = 0;
-        bool     enabled;
-        
-        } effect_config;
-        
-        void beginPixels(void);
+
+        /* Low level functions */
         void fillPixels(int color_R, int color_G, int color_B, int start_led, int count_led );
         void setPixelsColor(int led, int color_R, int color_G, int color_B);
         void showPixels(void);
-        
-     
+        void showFillPixels(int color_R, int color_G, int color_B, int start_led, int end_led);
+
+
+        /* Effects management functions */
+        String   id_assigned = "";
+        void assign_id(String id_assigned);
+        uint8_t update(struct LedProperties effect_config);
+        bool checkUpdates(void);
+        void updateLedZone(struct LedProperties effect_config);
+        void updateEffectConfig(struct LedProperties effect_config);
+        void resetEffectConfig (void);
+        void cleanCurrentLedZone(void);
+        void removeEffect(void);
   };
 
  #endif
