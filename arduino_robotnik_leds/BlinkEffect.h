f10b28a307bac4cf97093f7ca902ce45c293be03
robert <rvasquez@robotnik.es>
2020-05-05 14:48:48 +0200

    Implemented fade property

--- arduino_robotnik_leds/BlinkEffect.h
+++ arduino_robotnik_leds/BlinkEffect.h
@@ -15,6 +15,13 @@
       bool isOn = false;         //Indica si el el led debe estar encendido o apagado
       //elapsedMillis blink_time;  //Tiempo transcurrido entre un intervalo
       float blink_time = 0;
+      enum blink_resources {FADE_IN, LED_ON, FADE_OUT, LED_OFF, HOLD_ON, HOLD_OFF};
+      int state = FADE_IN;
+      float fade_R = 0, fade_G = 0, fade_B = 0, fade_W = 0;
+      int fade_in_tics = 0, fade_out_tics = 0;
+      int fade_in_timeout = 0, fade_out_timeout = 0;
+      float increment_fade_in_R, increment_fade_in_G, increment_fade_in_B, increment_fade_in_W;
+      float increment_fade_out_R, increment_fade_out_G, increment_fade_out_B, increment_fade_out_W;
       
     public:
     
